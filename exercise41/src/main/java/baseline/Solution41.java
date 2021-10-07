6878dfaa8c7aaed7dc00226ced60231a6397cdd1
Sue Lin <r.lin13@knights.ucf.edu>
2021-10-07 01:08:06 -0400

    first commit

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Sue Lin
+ */
+
+package baseline;
+
+/*
+*Create a program that reads in the following list of names from a file called `exercise41_input.txt` and sorts the list alphabetically:
+ */
+public class Solution41 {
+
+    public static void main (String[] args){
+        //start by creating a file path that is going to the parameter
+        //open the file
+        //then create a buffer and an array for the file
+        //create a string that will store the elements read in
+        //loop it so that while the file doesn't end it'll read everything in and store it in an array
+        //then call a function that will sort the names
+        //then call a function to print out the results
+        //close the file
+    }
+
+    //function to sort the array
+    //maybe make it into a seperate class? idk yet
+    void nameSorter(){
+        //use sort function to sort the names
+    }
+}
