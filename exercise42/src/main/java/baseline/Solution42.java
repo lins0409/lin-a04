9cc0a929b3d68b8eb517ee171c033ba8609b397e
Sue Lin <r.lin13@knights.ucf.edu>
2021-10-09 00:34:20 -0400

    first commit

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Sue Lin
+ */
+package baseline;
+import java.util.Scanner;
+
+/*
+*Sometimes data comes in as a structured format that you have to break down and turn into records so you can process them. CSV, or comma-separated values, is a common standard for doing this.
+*
+*Construct a program that reads in the following data file
+ */
+public class Solution42 {
+
+    public static void main (String[] args){
+        //set the scanner to the file that the program needs to read
+        //scan in the file and separate each bit into their own pieces (last name, first name, salary)
+        //store it into an array list?
+        //send the list into the printing function
+    }
+
+    //printing out the output, make sure it's formatted correctly
+    //may change it into a string and just have it return the current list to main
+    void printOutput(){
+        //take in the list, then print it out with spacing
+    }
+}
