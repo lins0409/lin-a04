ec87a0da4002271d61471cab6cf249418be04d37
Sue Lin <r.lin13@knights.ucf.edu>
2021-10-10 22:42:57 -0400

    updated output formatting

--- exercise42/src/main/java/baseline/Solution42.java
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -3,6 +3,12 @@
  *  Copyright 2021 Sue Lin
  */
 package baseline;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /*
@@ -11,17 +17,54 @@ import java.util.Scanner;
 *Construct a program that reads in the following data file
  */
 public class Solution42 {
+    //Initialize function caller
+    public static final  Solution42 sol = new Solution42();
 
-    public static void main (String[] args){
+    public static void main (String[] args) throws IOException {
         //set the scanner to the file that the program needs to read
+        Scanner inputFile = new Scanner(new File("./data/exercise42_input.txt"));
+
+        //this is to test to make sure that the file was opened without an issue
+        if (inputFile != null){
+            System.out.println("Successfully opened file\n");
+        }
+        else {
+            System.out.println("Error opening file\n");
+        }
+
+        //create an array to store the names
+        ArrayList<String> names = new ArrayList<String>();
+
         //scan in the file and separate each bit into their own pieces (last name, first name, salary)
+        while (inputFile.hasNextLine()) {
+            names.add(inputFile.nextLine());
+        }
+
         //store it into an array list?
         //send the list into the printing function
+        sol.printBeginningOfOutput(names);
+
+        //close the input file
+        inputFile.close();
     }
 
     //printing out the output, make sure it's formatted correctly
     //may change it into a string and just have it return the current list to main
-    void printOutput(){
+    void printBeginningOfOutput(ArrayList <String> names) throws IOException {
+        FileWriter fileWriter = new FileWriter("./data/exercise42_output.txt");
         //take in the list, then print it out with spacing
+        fileWriter.write("Last      First     Salary\n");
+        fileWriter.write("--------------------------\n");
+
+        for (String i : names){
+            String[] tempString = i.split(",");
+            String formatLastName = String.format("%-10s", tempString[0]);
+            String formatFirstName = String.format("%-10s", tempString[1]);
+            String formatSalary = String.format("%-11s", tempString[2]);
+            fileWriter.write(formatLastName + formatFirstName + formatSalary + "\n");
+        }
+
+        //close the file otherwise it won't actually print
+        fileWriter.close();
     }
 }
--- exercise42/src/test/java/baseline/Solution42Test.java
+++ exercise42/src/test/java/baseline/Solution42Test.java
@@ -1,4 +1,7 @@
+package baseline;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class Solution42Test {
-  
+
 }
