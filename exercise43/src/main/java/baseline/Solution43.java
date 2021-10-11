d2f55f9cb5d9ef39d2dd8ac0ffa9a5c8a59fc133
Sue Lin <r.lin13@knights.ucf.edu>
2021-10-10 23:58:57 -0400

    first commit

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,50 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Sue Lin
+ */
+
+package baseline;
+import java.util.Scanner;
+
+/*
+*Programming languages can create files and folders too.
+*
+*Create a program that generates a website skeleton with the following specifications:
+**
+*Prompt for the name of the site.
+*Prompt for the author of the site.
+*Ask if the user wants a folder for JavaScript files.
+*Ask if the user wants a folder for CSS files.
+*Generate an index.html file that contains the name of the site inside the <title> tag and the author in a <meta> tag.
+ */
+public class Solution43 {
+    //initialize scanner
+
+    public static void main(String[] args){
+        //prompt user for name of the website
+        //scan it in and store the value
+        //prompt user to enter the author of the site
+        //scan it in and store the value
+        //set a boolean to true or false depending on the value
+        //ask the user if they want to create a JavaScript file
+        //scan it in and store the value
+        //set the boolean to true or false depending on the value read in
+        //ask if the user wants to create a folder for CSS files
+        //call the file creator function
+    }
+
+    //pass in the bool
+    void createJavaScriptFile(boolean status){
+        //if the bool is true, have the program create a new file in the directory
+    }
+
+    //pass in the bool
+    void createCSSFile(boolean status){
+        //if the bool is true, have the program create a new file in the directory
+    }
+
+    //makes html file
+    void createHTML(){
+        //create a new file in the directory
+    }
+}
