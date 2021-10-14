91fbc4b8f8b2607bcbc496807e34a4b5c912503c
Sue Lin <r.lin13@knights.ucf.edu>
2021-10-14 00:22:19 -0400

    first commit

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Sue Lin
+ */
+
+package baseline;
+
+/*
+*Pulling data from a file into a complex data structure makes parsing much simpler. Many programming languages support the JSON format, a popular way of representing data.
+*
+*Create a program that takes a product name as input and retrieves the current price and quantity for that product. The product data is in a data file in the JSON format  and looks like this
+*
+ */
+
+public class Solution44 {
+    //initialize scanner
+
+    public static void main (String[] args){
+        //create a new JSON object or parser
+        //prompt user for the product name
+        //save the name in a string
+        //call a function to find the object in the json file
+        //if the item isn't on the list, print out that it doesn't exist
+        //if it does, print out the name, price, and quantity
+    }
+
+    //pass in the string that has the saved name
+    void JSONReader(){
+        //create a JSONParser
+        //put it into a try
+        //direct the parser into the right file
+        //create a buffer for reading
+        //loop until the name matches or until it reaches the end of the file and can't find it
+        //return the results
+    }
+}
