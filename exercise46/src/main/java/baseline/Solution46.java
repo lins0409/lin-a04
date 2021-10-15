b2fd3fcc16f31e97162bde537ecb7c0b9aba1ed1
Sue Lin <r.lin13@knights.ucf.edu>
2021-10-15 00:41:05 -0400

    first commit

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Sue Lin
+ */
+
+package baseline;
+
+/*
+*Knowing how often a word appears in a sentence or block of text is helpful for creating word clouds and other types of word analysis. And it’s more useful when running it against lots of text.
+*
+*Create a program that reads in a file named `exercise46_input.txt` and counts the frequency of words in the file. Then construct a histogram displaying the words and the frequency, and display the histogram to the screen.
+ */
+public class Solution46 {
+
+    public static void main (String[] args){
+        //initialize scanner and calls instance
+        //create a new file
+        //read in the input file, pass it into the readFile function
+        //call readFile function
+        //call printResults function
+        //close file
+    }
+
+    void readFile(){
+        //create a map or array for the information
+        //use the scanner to read the lines of input
+        //while the scanner has a line, keep add 1 to the count
+        //place the values into the map once it is done
+
+    }
+
+    //printing function
+    void printResults(){
+        //create arrays and strings to store the values from the file
+        //sort the values
+        //print out the item and then follow it with an astrisk
+    }
+}
