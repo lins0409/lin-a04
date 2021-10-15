d70cdb24fff2f12a07e3126890fd62338aaff970
Sue Lin <r.lin13@knights.ucf.edu>
2021-10-14 20:26:34 -0400

    first commit

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Sue Lin
+ */
+package baseline;
+
+/*
+*There will be times when you'll need to read in one file, modify it, and then write a modified version of that file to a new file.
+ */
+public class Solution45 {
+
+    public static void main (String[] args){
+        //initialize scanner
+        //create an array to store the text in the file
+        //prompt user for name of the file to read
+        //scan in the name and store in string
+        //call function to write output
+    }
+
+    void writeNewOutput(){
+        //direct the output file to the correct location and make sure it is names properly
+        //make a loop so that as long as it is not the end of the file it will find all instances where the word utilized is called, and replace it with use.
+        //close file
+    }
+}
