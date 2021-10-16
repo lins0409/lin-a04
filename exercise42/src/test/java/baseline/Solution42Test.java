2cd4c56ae30cfb471005cfacb8256b9d99b4268e
Sue Lin <r.lin13@knights.ucf.edu>
2021-10-16 02:08:46 -0400

    updated test

--- exercise42/src/test/java/baseline/Solution42Test.java
+++ exercise42/src/test/java/baseline/Solution42Test.java
@@ -1,7 +1,32 @@
 package baseline;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Solution42Test {
 
+    @Test
+    void testAdding() throws FileNotFoundException {
+        addEmployees emp = new addEmployees();
+        ArrayList <String> names = new ArrayList<>();
+        Scanner inputFile = new Scanner(new File("./data/exercise42_input.txt"));
+        ArrayList<String> actual = emp.addToList(inputFile, names);
+
+        ArrayList <String> expected = new ArrayList<>();
+        expected.add("Ling,Mai,55900");
+        expected.add("Johnson,Jim,56500");
+        expected.add("Jones,Aaron,46000");
+        expected.add("Jones,Chris,34500");
+        expected.add("Swift,Geoffrey,14200");
+        expected.add("Xiong,Fong,65000");
+        expected.add("Zarnecki,Sabrina,51500");
+
+        assertEquals(expected, actual);
+    }
 }
