e96b8f0abaebfa6ef0fe8c55aad2507c792bc224
Sue Lin <r.lin13@knights.ucf.edu>
2021-10-17 01:26:08 -0400

    completely rewriting the program logic but i'm so tired

--- exercise44/src/main/java/baseline/Solution44.java
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -9,10 +9,9 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import java.io.FileReader;
+
+import java.io.*;
 import java.util.Iterator;
-import java.util.Objects;
-import java.util.Scanner;
 
 /*
 *Pulling data from a file into a complex data structure makes parsing much simpler. Many programming languages support the JSON format, a popular way of representing data.
@@ -22,66 +21,20 @@ import java.util.Scanner;
  */
 public class Solution44 {
 
-    public static void main (String[] args){
-        //import scanner
-        Scanner scanner = new Scanner(System.in);
+    public static void main (String[] args) {
         //initialize instance of Solution 44 class
         Solution44 sol = new Solution44();
 
-        //prompt user for the product name
-        System.out.print("What is the product name? ");
-        //save the name in a string
-        String product = scanner.nextLine();
-
-        //call a function to find the object in the json file
-        String findings = sol.JsonReader(product);
-        System.out.print(findings);
-
-        if(Objects.equals(findings, "Sorry, that product was not found in our inventory.\n")){
-            sol.mainCaller();
-        }
-        else {
-            System.exit(0);
-        }
+        sol.JsonReader();
     }
 
-    void mainCaller (){
+    //call it to repeat until it makes the program exit
+    void mainCaller () throws IOException {
         main(null);
     }
 
     //pass in the string that has the saved name
-    String JsonReader(String product){
-        String results = " ";
+    void JsonReader() {
 
-        //put it into a try
-        try {
-            //direct the parser into the right file
-            Object obj = JsonParser.parseReader(new FileReader("./data/exercise44_input.json"));
-            //create JSONObject to get the file
-            JsonObject jsonObj = (JsonObject) obj;
-            JsonArray items = (JsonArray) jsonObj.get("products");
-
-            //create an iterator for looping
-            Iterator<JsonElement> iterator = items.iterator();
-
-            while (iterator.hasNext()) {
-                JsonObject object = (JsonObject) iterator.next();
-                String productName = object.get("name").toString();
-
-                //loop until the name matches or until it reaches the end of the file and can't find it
-                if (product.equalsIgnoreCase(productName)) {
-                    //if the item exists, print out the name, price, and quantity
-                    results = "Name: " + productName + "\nPrice: " + jsonObj.get("price") + "\nQuantity: " + jsonObj.get("quantity");
-                } else {
-                    //if the item isn't on the list, print out that it doesn't exist
-                    results = "Sorry, that product was not found in our inventory.\n";
-                }
-            }
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return results;
     }
 }
